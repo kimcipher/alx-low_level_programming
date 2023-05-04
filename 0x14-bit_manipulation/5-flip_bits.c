00f849059dc1d04f5c59026ebb357b3f4868c6a1
kimcipher <kimutaij192@gmail.com>
2023-05-04 16:07:49 +0300

    jamaa

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,12 +12,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
   unsigned int count = 0;
 
-  if (n < 0 && m >= 0) {
-    return -1;
-  } else if (n >= 0 && m < 0) {
-    return -1;
-  }
-
   while (n != m) {
     if (n & 1) {
       m ^= 1;
@@ -29,6 +23,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
   return count;
 }
-
 
 
