e21d6c96fafcf4af25028af526eead9919598df7
kimcipher <kimutaij192@gmail.com>
2023-05-04 15:39:44 +0300

    removal of print bin

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * print_bin - prints binary rep of @n
- *
- * @number: decimal value
- *
- * Return: nothing
-*/
-void print_bin(unsigned long int number)
-{
-	if (number >> 1)
-		print_bin(number >> 1);
-
-	putc((number & 1) ? '1' : '0', stdout);
-}
-
-/**
  * flip_bits - a function that returns the number of bits you
  *             would need to flip to get from one number to
  *             another
