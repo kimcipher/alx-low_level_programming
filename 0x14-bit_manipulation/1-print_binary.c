5f38e4ddd55e8e1b9877247d828b0beb2644915b
kimcipher <kimutaij192@gmail.com>
2023-05-04 16:21:21 +0300

    error handling

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,50 +1,16 @@
+#include "main.h"
 
 /**
- * _length - find the length of @n
+ * print_binary - prints the binary representation of a number
  *
- * @n: value to find its length
+ * @n: number to be printed in binary
  *
- * Return: length
-*/
-int _length(unsigned long int n)
-{
-	int length = 0;
-
-	while (n > 0)
-	{
-		length++;
-		n >>= 1; /*shift n to the right by 1*/
-	}
-
-	length--;
-
-	return (length);
-}
-
-/**
- * print_binary - a function that converts a decimal to binary
- *
- * @n: decimal number to convert
- *
- * Return: nothing
-*/
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-	int length;
-	unsigned long int mask = 1;
+    if (n > 1)
+        print_binary(n >> 1);
 
-	length = _length(n);
-
-	if (length > 0) /*create mask based on length of number*/
-		mask <<= length; /*shift mask to the left by length*/
-
-	while (mask > 0)
-	{
-		if (n & mask) /*if n & mask == 1 print 1*/
-			_putchar('1');
-		else /*else if n & mask == 0 print 0*/
-			_putchar('0');
-
-		mask >>= 1; /*shift mask to the right by 1*/
-	}
+    _putchar((n & 1) + '0');
 }
