ae151acec6bf070ffab5cfacc7562ba618413448
kimcipher <kimutaij192@gmail.com>
2023-05-04 15:47:35 +0300

    checks and handling cases

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,6 +14,25 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
   unsigned int count = 0;
 
+  // check that n and m are positive integers
+  if (n <= 0 || m <= 0) {
+	return 0; // Return an error or default value
+  }
+
+  // Check that n and m have the same number of bits
+  if (sizeof(n)*8 != sizeof(m)*8) {
+    return 0; // Return an error or default value
+  }
+
+  // Pad the smaller integer with zeros if necessary
+  if (n < m) {
+    n <<= sizeof(m)*8 - sizeof(n)*8;
+  } else if (m < n) {
+    m <<= sizeof(n)*8 - sizeof(m)*8;
+  }
+
+
+	// Count the number of bits that need to be flipped
   while (n != m) {
     if (n & 1) {
       m ^= 1;
