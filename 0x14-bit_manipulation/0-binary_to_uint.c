b5b37c3e6d975c040e4591917e9bace02b786596
kimcipher <kimutaij192@gmail.com>
2023-05-04 15:21:59 +0300

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -39,23 +39,19 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 	unsigned int base = 1;
-	int i, len;
+	int i;
 
-	if (b == NULL)
-		return (0);
+  if (b == NULL) {
+    return 0;
+  }
 
-	len = strlen(b);
-	for (i = len - 1; i >= 0; i--)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
+  for (i = strlen(b) - 1; i >= 0; i--) {
+    if (b[i] != '0' && b[i] != '1') {
+      return 0;
+    }
+    result = result * 2 + (b[i] - '0');
+  }
 
-		if (b[i] == '1')
-			result += base;
-
-		base *= 2;
-	}
-
-	return (result);
+  return result;
 }
 
