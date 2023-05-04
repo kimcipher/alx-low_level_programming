28ea3582ab0f9ebfe5e2096ee0039eafb542e6c6
kimcipher <kimutaij192@gmail.com>
2023-05-04 17:01:35 +0300

    para=enthesis

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -11,7 +11,7 @@
 int sum_listint(listint_t *head)
 {
 	if (head == NULL)
-		return 0;
+		return (0);
 	else
-		return head->n + sum_listint(head->next);
+		return (head->n + sum_listint(head->next));
 }
