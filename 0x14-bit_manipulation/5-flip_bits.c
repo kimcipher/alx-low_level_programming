fef21c62c68267fd60e22efcd4619278e5a7d513
kimcipher <kimutaij192@gmail.com>
2023-05-04 15:51:40 +0300

    revert

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,9 +14,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
   unsigned int count = 0;
 
-  
-
-
 	// Count the number of bits that need to be flipped
   while (n != m) {
     if (n & 1) {
@@ -24,7 +21,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
       count++;
     }
     n >>= 1;
-	m >>= 1;
   }
 
   return count;
