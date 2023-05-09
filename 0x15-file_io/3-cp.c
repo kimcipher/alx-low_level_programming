d545dc07b37115a77b4385138395771cbd684664
kimcipher <kimutaij192@gmail.com>
2023-05-09 16:36:18 +0300

    3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,86 +1,74 @@
 #include "main.h"
 
-#define MAXSIZE 1024
-
-
 /**
- * __exit - prints error messages and exits with exit number
- *
- * @error: either the exit number or file descriptor
- * @str: name of either file_in or file_out
- * @fd: file descriptor
- *
- * Return: 0 on success
-*/
-int __exit(int error, char *str, int fd)
+ * cls - error check
+ * @file1_err: error in closing file1
+ * @file1: file descriptor
+ * @file2_err: error in closing file2
+ * @file2: file descriptor
+ */
+
+void	cls(int file1_err, int file1, int file2_err, int file2)
 {
-	switch (error)
+	if (file1_err == -1)
 	{
-		case 97:
-			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-			exit(error);
-		case 98:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-			exit(error);
-		case 99:
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-			exit(error);
-		case 100:
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-			exit(error);
-		default:
-			return (0);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file1);
+		exit(100);
+	}
+	if (file2_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file2);
+		exit(100);
 	}
 }
 
 /**
- * main - create a copy of file
- *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: 0 for success.
-*/
+ * main - entry point
+ * @argc: arguments count
+ * @argv: pointer to array of arguments
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
-	int file_in, file_out;
-	int read_stat, write_stat;
-	int close_in, close_out;
-	char buffer[MAXSIZE];
+	int	file1, file1_err, wrt;
+	int	file2, file2_err, rd = 1024;
+	char	buff[1024];
 
-	/*if arguments are not 3*/
 	if (argc != 3)
-		__exit(97, NULL, 0);
-
-	/*sets file descriptor for copy from file*/
-	file_in = open(argv[1], O_RDONLY);
-	if (file_in == -1)
-		__exit(98, argv[1], 0);
-
-	/*sets file descriptor for copy to file*/
-	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_out == -1)
-		__exit(99, argv[2], 0);
-
-	/*reads file_in as long as its not NULL*/
-	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
 	{
-		if (read_stat == -1)
-			__exit(98, argv[1], 0);
-
-		/*copy and write contents to file_out*/
-		write_stat = write(file_out, buffer, read_stat);
-		if (write_stat == -1)
-			__exit(99, argv[2], 0);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
-
-	close_in = close(file_in); /*close file_in*/
-	if (close_in == -1)
-		__exit(100, NULL, file_in);
-
-	close_out = close(file_out); /*close file_out*/
-	if (close_out == -1)
-		__exit(100, NULL, file_out);
-
+	file1 = open(argv[1], O_RDONLY);
+	if (file1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		return (99);
+	}
+	while (rd == 1024)
+	{
+		rd = read(file1, buff, 1024);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			return (98);
+		}
+		wrt = write(file2, buff, rd);
+		if (wrt == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			return (99);
+		}
+	}
+	file1_err = close(file1);
+	file2_err = close(file2);
+	cls(file1_err, file1, file2_err, file2);
 	return (0);
 }
