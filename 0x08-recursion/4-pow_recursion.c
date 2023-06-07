8410bc1f3dc4ee78cc980b652f2dba4a43ba12af
kimcipher <kimutaij192@gmail.com>
2023-06-07 09:34:42 +0300

    betty pow recursion style

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -20,5 +20,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 
-	return (x * _pow_recursion(x, y - 1)); /* Recursive call to calculate the power */
+	return (x * _pow_recursion(x, y - 1)); /* Recursive call for power calculation */
 }
