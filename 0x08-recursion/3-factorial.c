d49ff394170e69a45dcefd3e526fd913b8884911
kimcipher <kimutaij192@gmail.com>
2023-06-07 09:30:50 +0300

    parenthesis add

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,13 +10,13 @@ int factorial(int n)
 {
 	if (n < 0)  /* If n is negative, return -1 to indicate an error */
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (n == 0)  /* Base case: factorial of 0 is 1 */
 	{
-		return 1;
+		return (1);
 	}
 
-	return n * factorial(n - 1);  /* Recursive call to calculate the factorial */
+	return (n * factorial(n - 1));  /* Recursive call to calculate the factorial */
 }
