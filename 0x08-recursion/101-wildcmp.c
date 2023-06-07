5d4116df1b90cfebd7b2e300090b41cd7223917a
kimcipher <kimutaij192@gmail.com>
2023-06-07 09:50:19 +0300

    betty style

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,18 +9,18 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '\0') /* * at the end of s2 */
-            return (1);
-        if (*(s2 + 1) == '*') /* Skip consecutive '*' characters */
-            return (wildcmp(s1, s2 + 1));
-        if (*s1 == '\0') /* s1 is empty, but s2 has more characters */
-            return (wildcmp(s1, s2 + 1));
-        if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)) /* Match any string or skip '*' in s2 */
-            return (1);
-    }
-    if (*s1 != '\0' && *s2 != '\0' && (*s1 == *s2 || *s2 == '?'))
-        return (wildcmp(s1 + 1, s2 + 1));
-    return (*s1 == '\0' && *s2 == '\0'); /* Check if both strings are at the end */
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0') /* * at the end of s2 */
+			return (1);
+		if (*(s2 + 1) == '*') /* Skip consecutive '*' characters */
+			return (wildcmp(s1, s2 + 1));
+		if (*s1 == '\0') /* s1 is empty, but s2 has more characters */
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)) /* Match any string or skip '*' in s2 */
+			return (1);
+	}
+	if (*s1 != '\0' && *s2 != '\0' && (*s1 == *s2 || *s2 == '?'))
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (*s1 == '\0' && *s2 == '\0'); /* Check if both strings are at the end */
 }
