3b9d92f325874db540667fd7df6cd146a116c94e
kimcipher <kimutaij192@gmail.com>
2023-06-07 09:47:45 +0300

    wildcmp correct function

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,11 +16,11 @@ int wildcmp(char *s1, char *s2)
         if (*(s2 + 1) == '*') /* Skip consecutive '*' characters */
             return (wildcmp(s1, s2 + 1));
         if (*s1 == '\0') /* s1 is empty, but s2 has more characters */
-            return (0);
+            return (wildcmp(s1, s2 + 1));
         if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)) /* Match any string or skip '*' in s2 */
             return (1);
     }
     if (*s1 != '\0' && *s2 != '\0' && (*s1 == *s2 || *s2 == '?'))
         return (wildcmp(s1 + 1, s2 + 1));
-    return (0);
+    return (*s1 == '\0' && *s2 == '\0'); /* Check if both strings are at the end */
 }
