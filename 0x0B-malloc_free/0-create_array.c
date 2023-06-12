d50c85b0c769c3480f362629e9f3f3890a3598b6
kimcipher <kimutaij192@gmail.com>
2023-06-12 22:25:19 +0300

    betty coding style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,18 +3,18 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+    char *array;
+    unsigned int i;
 
-	if (size == 0)
-		return NULL;
+    if (size == 0)
+        return NULL;
 
-	array = malloc(size * sizeof(char));
-	if (array == NULL)
-		return NULL;
+    array = malloc(size * sizeof(char));
+    if (array == NULL)
+        return NULL;
 
-	unsigned int i;
-	for (i = 0; i < size; i++)
-		array[i] = c;
+    for (i = 0; i < size; i++)
+        array[i] = c;
 
-	return array;
+    return array;
 }
