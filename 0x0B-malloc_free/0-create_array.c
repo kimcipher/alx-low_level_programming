8b911729597b8055fe932a7f287fd68e0a0eb9d0
kimcipher <kimutaij192@gmail.com>
2023-06-12 22:37:18 +0300

    reduced characters

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of characters and initializes it with a specific character.
+ * create_array - Creates an array of characters and initializes it with
+ *                a specific character.
  * @size: The size of the array.
  * @c: The character to initialize the array with.
  *
+ * Return: The pointer to the array, or NULL if it fails.
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int i;
+    char *array;
+    unsigned int i;
 
-	if (size == 0)
-		return (NULL);
+    if (size == 0)
+        return (NULL);
 
-	array = malloc(size * sizeof(char));
-	if (array == NULL)
-		return (NULL);
+    array = malloc(size * sizeof(char));
+    if (array == NULL)
+        return (NULL);
 
-	for (i = 0; i < size; i++)
-		array[i] = c;
+    for (i = 0; i < size; i++)
+        array[i] = c;
 
-	return (array);
+    return (array);
 }
